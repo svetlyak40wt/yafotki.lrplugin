f1877b6ebd6b775518ba9bbe5375e051d947d9fd
Alexander Artemenko <svetlyak.40wt@gmail.com>
2008-05-18 22:50:16 +0400

    Version 0.1.0

---
+++ Info.lua
@@ -0,0 +1,10 @@
+return {
+    LrSdkVersion = 1.3,
+    LrToolkitIdentifier = 'ru.svetlyak.lightroom.export.yafotki',
+    LrExportServiceProvider = {
+        title = LOC "$$$/Yandex/SProvider/Title=Yandex.Fotki",
+        file  = "YaExportServiceProvider.lua",
+        builtInPresetsDir = "Yandex",
+        image = 'fotki-logo.png',
+    },
+}
