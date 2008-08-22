2247afd9e0e5a21339dcb25c0917e598d93e611c
Alexander Artemenko <svetlyak.40wt@gmail.com>
2008-08-23 01:57:43 +0400

    README was updated. And new rule in the pack.sh, to not overwrite existed zip archive.

--- pack.sh
+++ pack.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 VERSION=$(head -1 YaFotki.README | sed 's/.*: //')
+ARCHIVE="YaFotki-$VERSION.zip"
 
 cd ..
 
-zip -r YaFotki-$VERSION.zip YaFotki.lrplugin -x YaFotki.lrplugin/.git/\*
+[ -f "$ARCHIVE" ] && echo "File \"$ARCHIVE\" already exists. Please, remove it first." && exit 1
+
+zip -r "$ARCHIVE" YaFotki.lrplugin -x YaFotki.lrplugin/.git/\*
